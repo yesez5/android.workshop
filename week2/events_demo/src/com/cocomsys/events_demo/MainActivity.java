04464f939651f7891394a895f4a992add72e5d14
Jimmy Sáenz <jsaenzr@outlook.com>
2014-03-22 09:45:37 -0600

    [ED] cambios en rotación de pantalla

--- week2/events_demo/src/com/cocomsys/events_demo/MainActivity.java
+++ week2/events_demo/src/com/cocomsys/events_demo/MainActivity.java
@@ -2,12 +2,20 @@ package com.cocomsys.events_demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = MainActivity.class.getSimpleName();
 	Button btnGreet;
+	TextView tvGreet;
+
+	final String GREET_KEY = "GreetKey";
+
 	/**
 	 * Called when the activity is first created.
 	 */
@@ -23,5 +31,19 @@ public class MainActivity extends Activity {
 				Toast.makeText(MainActivity.this, "hello from anonymous function", Toast.LENGTH_LONG).show();
 			}
 		});
+
+		tvGreet = (TextView)findViewById(R.id.tv_greet);
+
+		if(savedInstanceState != null && savedInstanceState.containsKey(GREET_KEY)){
+			Toast.makeText(this, "saved text: " + savedInstanceState.get(GREET_KEY), Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		String greet = String.valueOf(tvGreet.getText());
+		Log.i(TAG, "saving in bundle");
+		outState.putString(GREET_KEY, greet);
 	}
 }
